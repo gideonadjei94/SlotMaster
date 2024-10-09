23dd0a40b6075769b6d0cd5130d14831fa526fe5
gideonadjei <gideonadjei94@gmail.com>
2024-10-09 01:42:00 +0000

    Authentication

---
+++ src/app/components/AuthModal.tsx
@@ -0,0 +1,49 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import Logo from "@/app/Assets/logo.png";
+import Image from "next/image";
+import { signIn } from "../utils/auth";
+import { GitHubAuthButton, GoogleAuthButton } from "./SubmitButtons";
+
+export function AuthModal() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Try for Free</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[360px]">
+        <DialogHeader className="flex flex-row justify-center items-center gap-2">
+          <Image src={Logo} alt="Logo" className="size-10" />
+          <h4 className="text-2xl font-semibold">
+            Slot<span className="text-primary">Master</span>
+          </h4>
+        </DialogHeader>
+        <div className="flex flex-col mt-5 gap-3">
+          <form
+            action={async () => {
+              "use server";
+              await signIn("google");
+            }}
+            className="w-full"
+          >
+            <GoogleAuthButton />
+          </form>
+          <form
+            action={async () => {
+              "use server";
+              await signIn("github");
+            }}
+            className="w-full"
+          >
+            <GitHubAuthButton />
+          </form>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,13 +1,18 @@
 import Link from "next/link";
 import Image from "next/image";
-import Logo from "@/app/Assets/Logo.png";
+import Logo from "@/app/Assets/logo.png";
+import { AuthModal } from "./AuthModal";
 
 export function Navbar() {
   return (
     <div className="flex py-5 items-center justify-between ">
-      <Link href="/">
+      <Link href="/" className="flex items-center gap-2">
         <Image src={Logo} alt="Logo" className="size-10" />
+        <h4 className=" text-2xl font-semibold">
+          Slot<span className="text-blue-600">Master</span>
+        </h4>
       </Link>
+      <AuthModal />
     </div>
   );
 }
