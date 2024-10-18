b3ba22c47f1212bc4ee3886fbd0d756b24f77822
gideonadjei <gideonadjei94@gmail.com>
2024-10-18 15:24:29 +0000

    Nylas SetUp

---
+++ src/app/api/oauth/exchange/route.ts
@@ -0,0 +1,41 @@
+import prisma from "@/app/utils/db";
+import { requireUser } from "@/app/utils/hook";
+import { nylas, nylasConfig } from "@/app/utils/nylas";
+import { redirect } from "next/navigation";
+import { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const session = await requireUser();
+  const url = new URL(req.url);
+  const code = url.searchParams.get("code");
+
+  if (!code) {
+    return Response.json("No Authorization code returned from Nylas", {
+      status: 400,
+    });
+  }
+
+  try {
+    const response = await nylas.auth.exchangeCodeForToken({
+      clientSecret: nylasConfig.apiKey,
+      clientId: nylasConfig.clientId,
+      redirectUri: nylasConfig.redirectUri,
+      code: code,
+    });
+
+    const { grantId, email } = response;
+    await prisma.user.update({
+      where: {
+        id: session.user?.id,
+      },
+      data: {
+        grantId: grantId,
+        grantEmail: email,
+      },
+    });
+  } catch (error) {
+    console.log("Nylas Auth Error", error);
+  }
+
+  redirect("/dashboard");
+}
