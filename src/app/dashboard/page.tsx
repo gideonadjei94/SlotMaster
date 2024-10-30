297e1829bd736d2ca8036ca665664e77bdf0af10
gideonadjei <gideonadjei94@gmail.com>
2024-10-30 16:47:04 +0000

    Events Route

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,27 @@
 import { redirect } from "next/navigation";
 import { auth } from "../utils/auth";
 import { requireUser } from "../utils/hook";
+import prisma from "../utils/db";
 
+async function getData(userId: string) {
+  const data = await prisma.user?.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      userName: true,
+      eventType: {
+        select: {
+          id: true,
+          active: true,
+          title: true,
+          url: true,
+          duration: true,
+        },
+      },
+    },
+  });
+}
 export default async function DashboardPage() {
   const session = await requireUser();
 
